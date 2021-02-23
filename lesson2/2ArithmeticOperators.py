de29b6efb847a8b23e503db03412261ed77145d5
Katy Duncan <duncan.katya@gmail.com>
2021-02-23 14:48:13 -0500

    Work on arithmetic operators and quiz

---
+++ lesson2/2ArithmeticOperators.py
@@ -0,0 +1,30 @@
+# multiplication happens before addition
+print (1+2+3*3)
+
+# exponentiation (note NOT ^ -->bitwise XOR operator)
+print (3 ** 2)
+
+# modulo (gives the remainder)
+print( 9 % 2)
+
+# integer division (rounds DOWN to an integer)
+print (7//2)
+
+#Quiz
+print((23 + 32 + 64)/3)
+
+# Fill this in with an expression that calculates how many tiles are needed.
+print(9*7 + 5*7)
+print((9+5)*7)
+#You buy 17 packages of tiles containing 6 tiles each. How many tiles will be left over?
+print(17*6)
+print(102-98)
+print((17*6)%(9*7+5*7))
+
+#Well formatted / readability:
+print(((3+ 32))+ -15//2)
+print(-15//2)
+print(3 + 32 + (-15 // 2))
+
+print((17 - 6)%(5 + 2))
+print((17 - 6) % (5 + 2))
