7b9a2277eb0d7893a676a4d2e808874664ad1619
Katy Duncan <duncan.katya@gmail.com>
2021-02-24 09:47:58 -0500

    complete lesson 10 Booleans

---
+++ lesson2/10BoolsOperators.py
@@ -0,0 +1,33 @@
+the_sun_is_up = True
+the_sun_is_blue = False
+# print(the_sun_is_up == the_sun_is_blue)
+# False
+# print(the_sun_is_up != the_sun_is_blue)
+# True
+
+x = 42 > 43
+print(x)
+# False
+
+# Logical Operators
+# and, or, not
+age = 14
+is_teen = age > 12 and age < 20
+print(is_teen)
+# True
+
+# not
+age = 14
+not_teen = not (age > 12 and age < 20)
+print(not_teen)
+# False
+
+# QUIZ
+sf_population, sf_area = 864816, 231.89
+rio_population, rio_area = 6453682, 486.5
+
+san_francisco_pop_density = sf_population/sf_area
+rio_de_janeiro_pop_density = rio_population/rio_area
+
+# Write code that prints True if San Francisco is denser than Rio, and False otherwise
+print(san_francisco_pop_density > rio_de_janeiro_pop_density)
