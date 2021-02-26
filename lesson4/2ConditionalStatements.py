88e299419e354dfafa1fd3ce0de9c44e4a665582
Katy Duncan <duncan.katya@gmail.com>
2021-02-26 11:44:03 -0500

    complete lessons on conditional statements

--- lesson4/2ConditionalStatements.py
+++ lesson4/2ConditionalStatements.py
@@ -70,3 +70,127 @@ else:
 
 message = "Somebody who is {} years old will pay ${} to ride the bus.".format(age, ticket_price)
 print(message)
+
+
+# Practice: Which Prize
+points = 174
+
+if points <= 50:
+    result = "Congratulations! You won a wooden rabbit!"
+elif points <= 150:
+    result = "Oh dear, no prize this time."
+elif points <= 180:
+    result = "Congratulations! You won a wafer-thin mint!"
+else:
+    result = "Congratulations! You won a penguin!"
+
+print(result)
+
+
+# '''
+# You decide you want to play a game where you are hiding
+# a number from someone.  Store this number in a variable
+# called 'answer'.  Another user provides a number called
+# 'guess'.  By comparing guess to answer, you inform the user
+# if their guess is too high or too low.
+
+# Fill in the conditionals below to inform the user about how
+# their guess compares to the answer.
+# '''
+answer = 5 #provide answer
+guess = 4 #provide guess
+
+if guess < answer: #provide conditional
+    result = "Oops!  Your guess was too low."
+elif guess > answer: #provide conditional
+    result = "Oops!  Your guess was too high."
+elif guess == answer: #provide conditional
+    result = "Nice!  Your guess matched the answer!"
+
+print(result)
+
+# '''
+# Depending on where an individual is from we need to tax them
+# appropriately.  The states of CA, MN, and
+# NY have taxes of 7.5%, 9.5%, and 8.9% respectively.
+# Use this information to take the amount of a purchase and
+# the corresponding state to assure that they are taxed by the right
+# amount.
+# '''
+state = 'CA' #Either CA, MN, or NY
+purchase_amount = 100 #amount of purchase
+
+if state == 'CA': #provide conditional for checking state is CA
+    tax_amount = .075
+    total_cost = purchase_amount*(1+tax_amount)
+    result = "Since you're from {}, your total cost is {}.".format(state, total_cost)
+
+elif state == 'MN': #provide conditional for checking state is MN
+    tax_amount = .095
+    total_cost = purchase_amount*(1+tax_amount)
+    result = "Since you're from {}, your total cost is {}.".format(state, total_cost)
+
+elif state == 'NY': #provide conditional for checking state is NY
+    tax_amount = .089
+    total_cost = purchase_amount*(1+tax_amount)
+    result = "Since you're from {}, your total cost is {}.".format(state, total_cost)
+
+print(result)
+
+
+# Complex Boolean Expressions
+weight = 55
+# height = 164
+height = 1.5
+
+if 18.5 <= weight / height**2 < 25:
+    print("BMI is considered 'normal'")
+
+is_raining = True
+is_sunny = True
+if is_raining and is_sunny:
+    print("Is there a rainbow?")
+
+unsubscribed = False
+location = "CAN"
+if (not unsubscribed) and (location == "USA" or location == "CAN"):
+    print("send email")
+
+
+# True Value Testing
+errors = 3
+# errors = 0
+
+if errors:
+    print("There are " + str(errors) + " mistakes.")
+else:
+    print("No mistakes here!")
+
+errors = 3
+if errors:
+    print("You have {} errors to fix!".format(errors))
+else:
+    print("No errors to fix!")
+
+
+
+points = 174  # use this as input for your submission
+
+# establish the default prize value to None
+prize = None
+
+# use the points value to assign prizes to the correct prize names
+if points <= 50:
+    prize = "wooden rabbit"
+elif 151 <= points <= 180:
+    prize = "wafer-thin mint"
+elif points >= 181:
+    prize = "penguin"
+
+# use the truth value of prize to assign result to the correct prize
+if prize:
+    result = "Congratulations! You won a {}!".format(prize)
+else:
+    result = "Oh dear, no prize this time."
+
+print(result)
